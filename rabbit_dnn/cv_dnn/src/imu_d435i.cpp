b336fecefccd0c9fdbff5965d146a91c6b282fdb
SokhengDin <dinsokhengbds@gmail.com>
2023-03-22 15:14:03 +0700

    imu works

--- rabbit_dnn/cv_dnn/src/imu_d435i.cpp
+++ rabbit_dnn/cv_dnn/src/imu_d435i.cpp
@@ -5,14 +5,14 @@
 #include <rclcpp/logger.hpp>
 #include <std_msgs/msg/string.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
+#include <image_transport/image_transport.hpp>
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/image_encodings.hpp>
 #include "tf2/LinearMath/Quaternion.h"
 #include "geometry_msgs/msg/vector3.h"
 #include "tf2_ros/transform_broadcaster.h"
-#include <cv_bridge/cv_bridge.h>
+#include "cv_bridge/cv_bridge.h"
 #include <rclcpp/logger.hpp>
-#include <map>
 #include <cmath>
 #include <chrono>
 #include <tf2_ros/transform_listener.h>
@@ -21,11 +21,42 @@
 #define DEPTH_WIDTH 640
 #define DEPTH_HEIGHT 480
 #define DEPTH_FPS 6 
+#define PI_FL  3.141592f
 
+using namespace std::chrono_literals;
 
-struct vector3
-{
-	uint16_t x, y, z;
+struct float3 {
+    float x, y, z;
+    float3 operator*(float t)
+    {
+        return { x * t, y * t, z * t };
+    }
+
+    float3 operator-(float t)
+    {
+        return { x - t, y - t, z - t };
+    }
+
+    void operator*=(float t)
+    {
+        x = x * t;
+        y = y * t;
+        z = z * t;
+    }
+
+    void operator=(float3 other)
+    {
+        x = other.x;
+        y = other.y;
+        z = other.z;
+    }
+
+    void add(float t1, float t2, float t3)
+    {
+        x += t1;
+        y += t2;
+        z += t3;
+    }
 };
 
 class RealsenseNode: public rclcpp::Node
@@ -35,25 +66,24 @@ class RealsenseNode: public rclcpp::Node
 		: Node("realsense_node")
 		{
 
-			SetUpDevice();
 
-
-			image_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("camera/input/rgb", 10);
+			// image_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("camera/input/rgb", 10);
+			image_publisher_ = image_transport::create_publisher(this, "camera/input/rgb");
 			image_timer_ = this->create_wall_timer(
-					1/30ms, std::bind(&RealsenseNode::image_callback, this));
+					33ms, std::bind(&RealsenseNode::image_callback, this));
 
-			gyro_publisher_ = this->create_publisher<geometry_msgs::msg::Vector3("camera/gyro/info", 10);
+			gyro_publisher_ = this->create_publisher<geometry_msgs::msg::Vector3>("camera/gyro/info", 10);
 			gyro_timer_ = this->create_wall_timer(
-					1/100ms, std::bind(&RealsenseNode::gyro_callback, this));
+					10ms, std::bind(&RealsenseNode::gyro_callback, this));
 
-			accel_publisher_ = this->create_publisher<geometry_msgs::msg::Vector3("camera/accel/info", 10);
+			accel_publisher_ = this->create_publisher<geometry_msgs::msg::Vector3>("camera/accel/info", 10);
 			accel_timer_ = this->create_wall_timer(
-					1/50ms, std::bind(&RealsenseNode::accel_callback, this));
+					20ms, std::bind(&RealsenseNode::accel_callback, this));
+
+		SetUpDevice();
 
 		}
 	private:
-
-
 		void SetUpDevice()
 		{
 			config_.enable_stream(RS2_STREAM_COLOR, 640, 480, RS2_FORMAT_RGB8, fps_);
@@ -74,25 +104,25 @@ class RealsenseNode: public rclcpp::Node
 
 				if (motion && motion.get_profile().stream_type() == RS2_STREAM_ACCEL && motion.get_profile() == RS2_STREAM_ACCEL && motion.get_profile().format() == RS2_FORMAT_MOTION_XYZ32F)
 				{
-					rs2_vectr accel_data = motion.get_motion_data();
+					rs2_vector accel_data = motion.get_motion_data();
 
 					process_accel(accel_data);
 				}
 			});
 
 			// Start Data pipeline
-			pipe_.start(config_);
-			RCLCPP_INFO(logger_, "Pipeline has started!");
+			// pipe_.start(config_);
+			RCLCPP_INFO(this->get_logger(), "Pipeline has started!");
 		}
 
 
 		void gyro_callback()
 		{
 
-			geometry_msgs::msg::Vector3::SharedPtr gyro_data;
-			gyro_data->x = theta_gyro_.x;
-			gyro_data->y = theta_gyro_.y;
-			gyro_data->z = theta_gyro_.z;
+			auto gyro_data = geometry_msgs::msg::Vector3();
+			gyro_data.x = theta_gyro_.x;
+			gyro_data.y = theta_gyro_.y;
+			gyro_data.z = theta_gyro_.z;
 
 			gyro_publisher_->publish(gyro_data);
 
@@ -101,10 +131,10 @@ class RealsenseNode: public rclcpp::Node
 
 		void accel_callback()
 		{
-			geometry_msgs::msg::Vector3::SharedPtr accel_data;
-			accel_data->x = theta_accel_.x;
-			accel_data->y = theta_accel_.y;
-			accel_data->z = theta_accel_.z;
+			auto accel_data = geometry_msgs::msg::Vector3();
+			accel_data.x = theta_accel_.x;
+			accel_data.y = theta_accel_.y;
+			accel_data.z = theta_accel_.z;
 
 			accel_publisher_->publish(accel_data);
 
@@ -131,17 +161,17 @@ class RealsenseNode: public rclcpp::Node
 				img_msg->is_bigendian = false;
 				img_msg->step = width * bpp;
 				img_msg->header.frame_id = "camera_d345i";
-				img_msg->header.stamp = rclcpp::Time::now();
-				image_publisher_->(img_msg);
-
+				// img_msg->header.stamp = std::chrono::high_resolution_clock::now;
+				image_publisher_.publish(img_msg);
+			}
 		}
 		
 		void process_gyro(rs2_vector gyro_data, double ts)
-                {
+        {
                         if (firstGyro_)
                         {
                                 firstGyro_ = false;
-                                last_ts_gyro_ = tf;
+                                last_ts_gyro_ = ts;
                                 return;
                         }
                         float3 gyro_angle;
@@ -161,11 +191,10 @@ class RealsenseNode: public rclcpp::Node
                         // Apply the calculated change of angle to the current angle (theta)
                         std::lock_guard<std::mutex> lock(theta_mtx_);
                         theta_gyro_.add(-gyro_angle.z, -gyro_angle.y, gyro_angle.x);
-                }
-
+        }
 
 		void process_accel(rs2_vector accel_data)
-                {
+        {
                         // Holds the angle as calculated froma accelerometer data
                         float3 accel_angle;
 
@@ -188,22 +217,19 @@ class RealsenseNode: public rclcpp::Node
                                 theta_accel_.x = theta_accel_.x * alpha_ + accel_angle.x * (1-alpha_);
                                 theta_accel_.z = theta_accel_.z * alpha_ + accel_angle.z * (1-alpha_);
                         }
-                }
-
-	
-		
+        }
 
 
 		// Angle of camera
-        	float3 theta_gyro_;
+        float3 theta_gyro_;
 		float3 theta_accel_;
-        	std::mutex theta_mtx_;
+        std::mutex theta_mtx_;
 
-        	float alpha_ = 0.98f;
-        	bool firstGyro_ = true;
-        	bool firstAccel_ = true;
-        	// Keeps the arrivals time of previous frame
-        	double last_ts_gyro_ = 0;
+        float alpha_ = 0.98f;
+        bool firstGyro_ = true;
+        bool firstAccel_ = true;
+        // Keeps the arrivals time of previous frame
+        double last_ts_gyro_ = 0;
 
 
 		bool found_gyro_ = false;
@@ -225,15 +251,26 @@ class RealsenseNode: public rclcpp::Node
 
 			
 		// Create publisher
-		rclcpp::Logger logger_;
-		rclcpp::Publisher<sensor_msgs::msg::Image::SharedPtr image_publisher_;
-		rclcpp::Publisher<sensor_msgs::msg::CompressedImage::SharedPtr image_compressed_publisher_;
-		rclcpp::Publisher<geometry_msgs::msg::Vector3::SharedPtr gyro_publisher_;
-		rclcpp::Publisher<geometry_msgs::msg::Vector3::SharedPtr accel_publisher_;
-		rclcpp::TimerBase::SharedPtr timer_;
+		image_transport::Publisher image_publisher_;
+		rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr gyro_publisher_;
+		rclcpp::Publisher<geometry_msgs::msg::Vector3>::SharedPtr accel_publisher_;
+		rclcpp::TimerBase::SharedPtr image_timer_;
+		rclcpp::TimerBase::SharedPtr gyro_timer_;
+		rclcpp::TimerBase::SharedPtr accel_timer_;
+
+};
 
 
+
+int main(int argc, char* argv[])
+{
+	rclcpp::init(argc, argv);
+
+	auto imu_d435i = std::make_shared<RealsenseNode>();
+
+	rclcpp::spin(imu_d435i);
+
+	rclcpp::shutdown();
+
+	return 0;
 }
-
-
-
