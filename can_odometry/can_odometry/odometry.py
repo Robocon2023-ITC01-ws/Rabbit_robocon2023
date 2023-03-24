65b067f8d50257c01337a6769a68298fa6ee8833
panhachet <chetsokhpanha@gmail.com>
2023-03-24 14:12:17 +0700

    nearly closed

--- can_odometry/can_odometry/odometry.py
+++ can_odometry/can_odometry/odometry.py
@@ -1,13 +1,14 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
+from
 
 import can
 import numpy as np
 
 wheel_d = 0.058  # in meter
 d_wheel = 0.285 # in meter
-def positionXr( tick, dir, count, pos):
+def position( tick, dir, count, pos):
     new_count = tick
     if (dir == False):
         if(new_count <= count):
@@ -22,40 +23,6 @@ def positionXr( tick, dir, count, pos):
         else:
             diff = (65536 - count) + new_count 
         pos = np.pi * wheel_d * diff/8200 * (-1.0) + pos
-    count = new_count
-    return pos, count
-def positionXl( tick, dir, count, pos):
-    new_count = tick
-    if (dir == True):
-        if(new_count <= count):
-            diff = count - new_count
-        else:
-            diff = (65536 - new_count) + count
-        pos = np.pi * wheel_d * diff/8200 * (1.0) + pos
-
-    else:
-        if(new_count>=count):
-            diff = new_count - count
-        else:
-            diff = (65536 - count) + new_count 
-        pos = np.pi * wheel_d * diff/8200 * (-1.0) + pos
-    return pos, count
-def positionY( tick, dir, count, pos):
-    new_count = tick
-    if (dir == False):
-        if(new_count <= count):
-            diff = count - new_count
-        else:
-            diff = (65536 - new_count) + count
-        pos = np.pi * wheel_d * diff/8200 * (1.0) + pos
-
-    else:
-        if(new_count>=count):
-            diff = new_count - count
-        else:
-            diff = (65536 - count) + new_count 
-        pos = np.pi * wheel_d * diff/8200 * (-1.0) + pos
-
     count = new_count
     return pos, count
 
@@ -67,11 +34,14 @@ class ros_node(Node):
 
         self.encoder_tick = np.zeros(3)
         self.encoder_dir = np.zeros(3)
+        self.position = np.zeros(3)
         self.count = np.zeros(3)
         self.Xr = 0.0 # in meter
         self.Y = 0.0 # in meter
         self.Xl = 0.0 # in meter
         self.theta = 0.0 # in meter
+        self.store = np.zeros(3)
+        i = 0
         
 
         self.odom_pub = self.create_publisher(Float32MultiArray, 'odom', 10)
@@ -84,19 +54,25 @@ class ros_node(Node):
             self.encoder_dir[0] = (msg.data[6] & 0x04) == 0x04
             self.encoder_dir[1] = (msg.data[6] & 0x02) == 0x02
             self.encoder_dir[2] = (msg.data[6] & 0x01) == 0x01
-            self.Xr, self.count[0] = positionXr(self.encoder_tick[0], self.encoder_dir[0], self.count[0], self.Xr)
-            self.Xl, self.count[1] = positionXl(self.encoder_tick[1], self.encoder_dir[1], self.count[1], self.Xl)
-            self.Y, self.count[2] = positionY(self.encoder_tick[2], self.encoder_dir[2], self.count[2], self.Y)
-            self.theta = (self.Xr + self.Xl)/(2*d_wheel)
+            while(i <= 6):
+                self.store[0] = self.encoder_tick[0]
+                self.store[1] = self.encoder_tick[1]
+                self.store[2] = self.encoder_tick[2]
+                i = i +1
+                break;
+            self.encoder_tick[0] = self.encoder_tick[0] - self.store[0]
+            self.encoder_tick[1] = self.encoder_tick[1] - self.store[1]
+            self.encoder_tick[2] = self.encoder_tick[2] - self.store[2]
+            self.Xr, self.count[0] = position(self.encoder_tick[0], self.encoder_dir[0], self.count[0], self.Xr)
+            self.Xl, self.count[1] = position(self.encoder_tick[1], self.encoder_dir[1], self.count[1], self.Xl)
+            self.Y, self.count[2] = position(self.encoder_tick[2], self.encoder_dir[2], self.count[2], self.Y)
+            xl = -self.Xl
+            self.theta = (self.Xr - xl)/(2*d_wheel)*57.298
             pos_msg = Float32MultiArray()
-            pos_msg.data = [self.Xr, self.Y]
-            data_pos = np.array([
-                [self.Xr],
-                [-self.Y],
-                [0.0],
-                [self.Xl]
-            ])
-            print(data_pos)
+            pos_msg.data = [self.Xr, -self.Y, self.theta]
+
+            self.position = np.array([np.round(self.Xr,3), np.round(-self.Y,3), np.round(self.theta,3), xl])
+            print(self.position)
              
             
             self.odom_pub.publish(pos_msg)
